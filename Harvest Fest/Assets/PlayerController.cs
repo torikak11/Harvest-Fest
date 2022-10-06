a1432427ead3b73e0359e876cd0fc019346a4e22
Victoria Kratzke <victoriakratzke@gmail.com>
2022-10-06 15:58:20 +0100

    Updating character animations

--- Harvest Fest/Assets/PlayerController.cs
+++ Harvest Fest/Assets/PlayerController.cs
@@ -1,23 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public float moveSpeed = 1f;
+    public float collisionOffset = 0.05f;
+    public ContactFilter2D movementFilter;
+
     Vector2 movementInput;
-    RigidBody2D rb;
+    Animator animator;
+    Rigidbody2D rb;
+    List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<RigidBody2D>();   
+        rb = GetComponent<Rigidbody2D>(); 
+        animator = GetComponent<Animator>();  
     }
 
     private void FixedUpdate() {
         // If movement input is not 0, try to move
-        if(movementInput != Vector2.ZERO) {
-
+        if(movementInput != Vector2.zero) {
+            bool success = TryMove(movementInput);
+            if (!success) {
+                success = TryMove(new Vector2(movementInput.x, 0));
+                if (!success) {
+                    success = TryMove(new Vector2(0, movementInput.y));
+                }
+            }
+            animator.SetBool("isMoving", success);
         }
+        else {
+            animator.SetBool("isMoving", false);
+        }
+        
+    }
 
+    private bool TryMove(Vector2 direction) {
+        int count = rb.Cast(
+                direction, // X and Y values between -1 and 1 that represent the direction from the body to look for collisions
+                movementFilter, // The settings that determine where a collision can occur on such as layers to collide with
+                castCollisions, // List of collisions to store the found collisions into after the Cast is finished
+                moveSpeed * Time.fixedDeltaTime + collisionOffset); // The amount to cast equal to the movement plus an offset
+        if (count == 0) {
+            rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
     void OnMove(InputValue movementValue) {
